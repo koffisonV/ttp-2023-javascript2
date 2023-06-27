ecb86d67f0a5061de8964cc842327c38d9e9d395
Koffison V <Koffison29@gmail.com>
2023-06-26 22:49:06 -0400

    03_solution.js solved

--- src/03_alphabet_soup/03_solution.js
+++ src/03_alphabet_soup/03_solution.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function soup(phrase, bank) {
+  const charCount = {};
+
+  // Count the characters in the phrase
+  for (let char of phrase) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  // Check if there are enough characters in the bank
+  for (let char of bank) {
+    if (charCount[char]) {
+      charCount[char]--;
+    }
+  }
+  // Check if all characters in the phrase have been accounted for
+  for (let count of Object.values(charCount)) {
+    if (count > 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const phrase = 'helloworld';
+const bank = 'dlrowhlloeh';
+console.log(soup(phrase, bank));
+
+const phrase2 = 'abcdefg';
+const bank2 = 'abcd';
+console.log(soup(phrase2, bank2));
